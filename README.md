e7ce34d12bd5262f1fbeb160e5c993f2bb1dca6e
Philip Downer <phil@csldevices.co.uk>
2013-04-18 23:22:42 +0100

    Added the core project files, the first version of sweb.

--- README.md
+++ README.md
@@ -5,4 +5,22 @@ sweb (Simple WEb Browser) is a fullscreen web browser with no controls, it's des
 
 This project uses QT/Webkit and is based on one of the QT example projects (fancybrowser). The reason this project was created was that in the case where we have been wanting to just display a single web page, other web browsers running in fullscreen or kiosk mode were too demanding of system resources and we did not need many (if any) of the features offered by those browsers. So we trialed creating a simple fullscreen webkit based browser (sweb). It's worked great for us so far taking up far less system resources, although we're still testing it. When I mentioned this project to others they were very interested in using it, so here it is...
 
+Building and running
+====================
 
+This has been tested with libqt4 and libqt4.8, I'd recommend 4.8 several people have shown interest in running this on the Raspberry Pi, as the current distro of choice for the Pi is raspbian which is based on Debian Wheezy and comes with libqt4.8 it should be as simple as the following to get it to run:
+
+  sudo apt-get install libqt4-webkit
+  git clone https://github.com/csldevices/sweb.git
+  cd sweb
+  qmake
+  make
+  ./sweb
+
+I haven't tested the above on a raspberry pi specfically but it should work fine, for none debian based systems ignore the first step and instead install qt4 and libqt-webkit using your distributions favourite pacakge manager. I guess this should work on other platforms than Linux, ie. any platfrom the Qt toolkit runs on but again, not been tested.
+
+By default sweb tries to load whatever is running on port 80 of the local machine, but if you pass a url on the command line it will go to that instead, so:
+
+  ./sweb http://www.google.com
+
+There is no built in method to exit sweb so you'll need to kill it from a command line.
---
+++ mainwindow.cpp
@@ -0,0 +1,89 @@
+/****************************************************************************
+**
+** Copyright (C) 2012 Digia Plc and/or its subsidiary(-ies).
+** Contact: http://www.qt-project.org/legal
+**
+** This file is taken from one of the examples from the Qt Toolkit (fancybrowser)
+**
+** $QT_BEGIN_LICENSE:BSD$
+** You may use this file under the terms of the BSD license as follows:
+**
+** "Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are
+** met:
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in
+**     the documentation and/or other materials provided with the
+**     distribution.
+**   * Neither the name of Digia Plc and its Subsidiary(-ies) nor the names
+**     of its contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+/*
+ * Copyright (C) 2013 CSL Devices Ltd 
+ * Contact: phil@csldevices.co.uk
+ * 
+ * - Removed most of the functionality from the original project, i.e. all 
+ *   navigation buttons and most menu functions have been removed
+ * 
+ */
+
+
+#include <QtGui>
+#include <QtNetwork>
+#include <QtWebKit>
+#include "mainwindow.h"
+
+MainWindow::MainWindow(const QUrl& url)
+{
+    progress = 0;
+
+    QNetworkProxyFactory::setUseSystemConfiguration(true);
+
+    view = new QWebView(this);
+    view->load(url);
+
+
+    setCentralWidget(view);
+
+}
+
+void MainWindow::adjustTitle()
+{
+    if (progress <= 0 || progress >= 100)
+        setWindowTitle(view->title());
+    else
+        setWindowTitle(QString("%1 (%2%)").arg(view->title()).arg(progress));
+}
+
+void MainWindow::setProgress(int p)
+{
+    progress = p;
+    adjustTitle();
+}
+
+void MainWindow::finishLoading(bool)
+{
+    progress = 100;
+    adjustTitle();
+}
+
